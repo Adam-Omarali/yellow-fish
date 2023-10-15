a67787f657a8825571d9c6afd6e68a3c0c989207
Adam <adamomarali37@gmail.com>
2023-10-15 02:09:15 -0400

    take pictures:

--- app/index.js
+++ app/index.js
@@ -1,35 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
-import { useEffect } from 'react';
-import * as tf from '@tensorflow/tfjs';
-import '@tensorflow/tfjs-react-native';
-import { Image } from 'expo-image';
-import fish from './Lexigo.png'
-import Canvas from 'react-native-canvas';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-react-native";
+import { Image } from "expo-image";
+import fish from "./Lexigo.png";
+import Canvas from "react-native-canvas";
 
 export default function Home() {
-
-    useEffect(() => {
-        async function load(){
-            tf.ready();
-            console.log('ready')
-        }
-        load()
-    }, [])
-
-    function handleCanvas(canvas){
-      const ctx = canvas.getContext('2d');
-      ctx.fillStyle = 'red';
-      ctx.fillRect(0, 0, 0, 0);
+  useEffect(() => {
+    async function load() {
+      tf.ready();
+      console.log("ready");
     }
+    load();
+  }, []);
+
+  function handleCanvas(canvas) {
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 0, 0);
+  }
 
   return (
     <View style={styles.container}>
       <Text>Yellow Fish</Text>
-      <Image source={fish} style={{width: 100, height: 100}}/>
+      <Image source={fish} style={{ width: 100, height: 100 }} />
       <Link href="/translate">Translate</Link>
       <Link href="/prediction">Prediction</Link>
+      <Link href="/camera">Camera</Link>
       {/* <Canvas style={{
           width: 350,
           height: 200,
@@ -47,8 +47,8 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- app/prediction.js
+++ app/prediction.js
@@ -1,38 +1,45 @@
-import * as cocoSsd from '@tensorflow-models/coco-ssd';
-import * as tf from '@tensorflow/tfjs';
-import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
-import { Camera } from 'expo-camera';
-import React, { useEffect, useRef, useState } from 'react';
-import { Dimensions, LogBox, Platform, StyleSheet, View, Text } from 'react-native';
-import Canvas from 'react-native-canvas';
-import { Link } from 'expo-router'
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as tf from "@tensorflow/tfjs";
+import { cameraWithTensors } from "@tensorflow/tfjs-react-native";
+import { Camera } from "expo-camera";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Dimensions,
+  LogBox,
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+} from "react-native";
+import Canvas from "react-native-canvas";
+import { Link } from "expo-router";
+import { translate } from "./translate";
 
 const TensorCamera = cameraWithTensors(Camera);
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 export default function App() {
   const [model, setModel] = useState();
   let context = useRef();
   const canvasRef = useRef();
-  const [predictions, setPredicitions] = useState([])
+  const [predictions, setPredicitions] = useState([]);
 
- useEffect(() => {
-   console.log(model)
- }, [model])
- 
+  useEffect(() => {
+    console.log(model);
+  }, [model]);
 
   function handleCameraStream(images) {
     const loop = async () => {
       const nextImageTensor = images.next().value;
 
-      if (!model) throw new Error('no model');
-      if (!nextImageTensor) throw new Error('no next image tensor');
+      if (!model) throw new Error("no model");
+      if (!nextImageTensor) throw new Error("no next image tensor");
 
       model
         .detect(nextImageTensor)
         .then((predictions) => {
-          setPredicitions(predictions)
+          setPredicitions(predictions);
         })
         .catch((err) => {
           console.log(err);
@@ -40,7 +47,9 @@ export default function App() {
 
       requestAnimationFrame(loop);
     };
-    loop();
+    setInterval(() => {
+      loop();
+    }, 1000);
   }
 
   // function drawRectangle(
@@ -87,31 +96,31 @@ export default function App() {
   //   }
   // }
 
-  function handleCanvas(can){
+  function handleCanvas(can) {
     if (can) {
       can.width = width;
       can.height = height;
-      const ctx = can.getContext('2d');
-      ctx.strokeStyle = 'red';
-      ctx.fillStyle = 'red';
+      const ctx = can.getContext("2d");
+      ctx.strokeStyle = "red";
+      ctx.fillStyle = "red";
       // if(predictions.length > 0){
       //   ctx.clearRect(0, 0, 350, 700);
       // }
       // else{
       //   ctx.fillRect(0, 0, 350, 700);
       // }
-      for (let prediction in predictions){
-        if(prediction.hasOwnProperty('bbox')){
+      for (let prediction in predictions) {
+        if (prediction.hasOwnProperty("bbox")) {
           const [x, y, width, height] = prediction.bbox;
           // ctx.fillRect(x, y, width, height);
-          ctx.fillRect(0, 0, 350, 700)
+          ctx.fillRect(0, 0, 350, 700);
         }
       }
     }
-  };
+  }
 
   let textureDims;
-  Platform.OS === 'ios'
+  Platform.OS === "ios"
     ? (textureDims = { height: 1920, width: 1080 })
     : (textureDims = { height: 1200, width: 1600 });
 
@@ -135,43 +144,38 @@ export default function App() {
     })();
   }, []);
 
-  function handleCanvas2(canvas){
-    if(canvas){
-      const ctx = canvas.getContext('2d');
-      ctx.fillStyle = 'red';
-      ctx.fillRect(0, 0, 350, 700);
-    }
-  }
-
   return (
     <View style={styles.container}>
-      {model ? 
-      <View>      
-        <TensorCamera
-          // Standard Camera props
-          style={styles.camera}
-          type={Camera.Constants.Type.back}
-          // Tensor related props
-          cameraTextureHeight={textureDims.height}
-          cameraTextureWidth={textureDims.width}
-          resizeHeight={200}
-          resizeWidth={152}
-          resizeDepth={3}
-          onReady={handleCameraStream}
-          autorender={true}
-          useCustomShadersToResize={false}
-        />
-        {/* <Canvas style={styles.canvas} ref={handleCanvas} /> */}
-        <View>{predictions.map(prediction => {
-          if(prediction.hasOwnProperty('class')){
-            return <Text>{prediction.class}</Text>
-          }
-        })}
+      {model ? (
+        <View>
+          <TensorCamera
+            // Standard Camera props
+            style={styles.camera}
+            type={Camera.Constants.Type.back}
+            // Tensor related props
+            cameraTextureHeight={textureDims.height}
+            cameraTextureWidth={textureDims.width}
+            resizeHeight={200}
+            resizeWidth={152}
+            resizeDepth={3}
+            onReady={handleCameraStream}
+            autorender={true}
+            useCustomShadersToResize={false}
+          />
+          {/* <Canvas style={styles.canvas} ref={handleCanvas} /> */}
+          <View>
+            {predictions.map((prediction) => {
+              if (prediction.hasOwnProperty("class")) {
+                return (
+                  <Text>{JSON.stringify(translate(prediction.class))}</Text>
+                );
+              }
+            })}
+          </View>
         </View>
-      </View>
-      : 
-      <Text>{model}</Text>
-      }
+      ) : (
+        <Text>{model}</Text>
+      )}
       <Canvas style={styles.canvas} ref={handleCanvas} />
       <Link href="/home">Home</Link>
     </View>
@@ -181,11 +185,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   camera: {
-    width: '90%',
-    height: '90%',
+    width: "90%",
+    height: "90%",
   },
   canvas: {
     position: "absolute",
@@ -193,8 +197,8 @@ const styles = StyleSheet.create({
     right: 0,
     textAlign: "center",
     zindex: 10,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     borderWidth: 1,
   },
 });
